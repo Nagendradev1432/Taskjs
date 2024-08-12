502ab6144d9306a05ee7fde090c6554561528ef2
Nagendra1432 <nagendramejari@gmail.com>
2024-08-12 23:32:30 +0530

    message

---
+++ index2.js
@@ -0,0 +1,193 @@
+console.log(10+'5');
+
+
+
+let z=10;
+console.log(z+=5);
+
+
+
+console.log(10-null);
+
+
+
+let c=10;
+console.log(c*2);
+
+
+
+console.log(10/2);
+
+
+
+console.log(10%3);
+
+
+
+let d=10;
+console.log(d**2);
+
+
+
+console.log(10>5 && 5<10);
+
+
+
+console.log(10==='10');
+
+
+
+console.log(null??'default');
+
+
+
+console.log(10||'default');
+
+
+
+console.log(!true);
+
+
+
+console.log(10&5);
+
+
+
+console.log(10|5);
+
+
+
+console.log(10^5);
+
+
+
+
+
+let e=10;
+y=++e + --e;
+console.log(e,y);
+
+
+
+let i=10;
+let h=i++;
+console.log(i,h);
+
+
+
+var x=10;
+console.log(++x);
+
+
+
+
+let f=10;
+console.log(++x);
+console.log(x++);
+
+
+
+var x=10;
+var y=--x;
+console.log(x,y);
+
+
+
+var x=10;
+var y=x--;
+console.log(x,y);
+
+
+
+let a=10;
+result=x>5;
+console.log(y);
+
+
+
+
+var g =10;
+var result=g>5;
+console.log(result);
+
+
+
+
+var p=10;
+var result=p>5?("maybe"):("no");
+console.log(result);
+
+
+
+
+const obj={a:{b:'c'}};
+console.log(obj?.b?.c);
+
+
+
+
+
+
+var x=null;
+var y=x??("default");
+console.log(y);
+
+
+
+
+
+
+
+
+var x=undefined;
+var y=x??("default");
+console.log(y);
+
+
+
+
+
+
+
+
+
+
+
+var x=0;
+var y=x??("default");
+console.log(y);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var p="sahithi";
+if(p=="harish"){
+    console.log("Trainer of the out batch");
+}
+else if(p=="sahithi"){
+    console.log("program manager of the our batch");
+}
+else if(p=="Naresh"){
+    console.log("support trainer of the our batch");
+}
+else if(p=="Bhasker"){
+    console.log("super tax of the trainer our batch");
+}
+else if(p=="Mukesh"){
+    console.log("support trainer of the our batch");
+}
+
+
+    
+    
+    
